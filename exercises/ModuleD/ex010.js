501dce39e74d9568bbbdfc264ce2567ca62318fc
Joao Pedro Schiavoni <joaopschiavoni@gmail.com>
2025-03-06 17:16:24 -0300

    Create ex010.js

---
+++ exercises/ModuleD/ex010.js
@@ -0,0 +1,12 @@
+var vel = 50
+var vel = Number(vel)
+console.log()
+console.log(`A velocidade do seu carro é ${vel}Km/h`)
+if (vel> 60) {
+    console.log('Voce ultrapassou a velocidade permitida. MULTADO!')
+}
+else; {
+    console.log('Dirija sempre com segurança!')
+}
+console.log()
+console.log('Fim do programa')
